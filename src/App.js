0dc3b7dd03399693ce72be344a5ee62a11311d33
henrique <henriquecontact@gmail.com>
2024-09-05 14:51:05 -0300

    fix: add ProcessSection img

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import StartNow from './pages/ourProcess/components/StartNow';
 import Footer from './pages/ourProcess/components/Footer';
 import './App.css';
 import img_process_one from './img/Process-01.png';
+import img_process_two from './img/Process-02.png';
 
 function App() {
   return (
@@ -20,7 +21,7 @@ function App() {
         number="02"
         title="Construindo uma Comunidade Forte e Inclusiva"
         text="O Rio Crypto Hub é mais do que um evento; é uma comunidade viva e inclusiva onde entusiastas, desenvolvedores, investidores e curiosos se reúnem para compartilhar ideias, colaborar em projetos e expandir suas redes de contatos. Juntos, estamos fortalecendo o ecossistema Web3 no Rio de Janeiro."
-        imgSrc="./img/Process-02.png"
+        imgSrc={ img_process_two }
         isGray={true}
       />
       <ProcessSection
